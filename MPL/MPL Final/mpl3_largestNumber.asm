9d46fffc5b7efaa66c413c169a4f28bedc53d6b5
prathameshks <prathameshks2003@gmail.com>
2023-06-05 18:08:22 +0530

    largest number from array

--- MPL/MPL Final/mpl3_largestNumber.asm
+++ MPL/MPL Final/mpl3_largestNumber.asm
@@ -7,51 +7,72 @@
 %endmacro
 
 section .data
-    m1 db "ASM Program for Calculate and Displaying Lenght of Input String",10
+    m1 db "ASM Program to Find Largest Number from the given Numbers",10
     m1l equ $-m1
 
-    m2 db "Enter string:"
+    m2 db "Largest number is "
     m2l equ $-m2
 
-    m3 db "String Entered is ",10
+    m3 db "Elements of Array are ",10
     m3l equ $-m3
 
-    m4 db "Length of Entered string is ",10
-    m4l equ $-m4
+    arr db 11h,59h,33h,66h,44h
 
     new_line db 10
+    space db " "
 
 section .bss
-    buff resb 250
-    size equ $-buff
-
     count resb 1
-    num_buff resb 1
+    num_buff resb 1 
 
 section .text
 global _start
 _start:
     universal 1,1,m1,m1l
-
-    universal 1,1,m2,m2l
-    universal 0,0,buff,size
-    mov [count],rax
-
     universal 1,1,new_line,1
     universal 1,1,m3,m3l
-    universal 1,1,buff,[count]
+
+    mov byte[count],05
+    mov rsi,arr
+
+next:
+    mov rax,[rsi]
+    push rsi
+    call display
+    universal 1,1,space,1
+    pop rsi
+    inc rsi
+    dec byte[count]
+    jnz next 
 
     universal 1,1,new_line,1
-    universal 1,1,m4,m4l
-
+    universal 1,1,new_line,1
+    universal 1,1,m2,m2l
+    call findmax
+    
     call display
     universal 1,1,new_line,1
-    
+
     jmp exit
 
+
+findmax:
+    mov rsi,arr
+    mov byte[count],6
+    mov al,0h 
+
+    up2:
+        cmp al,[rsi]
+        jg skip2
+        mov al,[rsi]
+    skip2:
+        inc rsi
+        dec byte[count]
+        jnz up2
+    ret
+
+
 display:
-    mov rax,[count]
-    dec rax
     mov rdi,num_buff
 
     mov rcx,2
@@ -78,4 +99,4 @@ mov rax,60
 mov rdi,0
 syscall
 
-;nasm -f elf64 mpl2_lengthofstring.asm -o a.o ;ld a.o  -o a ;./a
+;nasm -f elf64 mpl3_largestNumber.asm -o a.o ;ld a.o  -o a ;./a
